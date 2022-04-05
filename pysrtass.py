b88f376108db5c61403e9b4ebe3c449c44d24e6d
SASUKE-DUCK <95662926+SASUKE-DUCK@users.noreply.github.com>
2022-04-05 18:46:02 -0500

    Add files via upload

---
+++ pysrtass.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# Module: VTT-TO-ASS/SSA
+# Created on: 05-04-2022
+# Authors: -∞WKS∞-
+# Version: 1.5.0
+
+import sys
+import os
+import re
+import codecs
+
+
+def fileopen(input_file):
+    encodings = ["utf-32", "utf-16", "utf-8", "cp1252", "gb2312", "gbk", "big5"]
+    tmp = ''
+    for enc in encodings:
+        try:
+            with codecs.open(input_file, mode="r", encoding=enc) as fd:
+                tmp = fd.read()
+                break
+        except:
+            # print enc + ' failed'
+            continue
+    return [tmp, enc]
+
+
+def srt2ass(input_file):
+    if '.ass' in input_file:
+        return input_file
+
+    if not os.path.isfile(input_file):
+        print(input_file + ' not exist')
+        return
+
+    src = fileopen(input_file)
+    tmp = src[0]
+    encoding = src[1]
+    src = ''
+    utf8bom = ''
+
+    if u'\ufeff' in tmp:
+        tmp = tmp.replace(u'\ufeff', '')
+        utf8bom = u'\ufeff'
+    
+    tmp = tmp.replace("\r", "")
+    lines = [x.strip() for x in tmp.split("\n") if x.strip()]
+    subLines = ''
+    tmpLines = ''
+    lineCount = 0
+    output_file = '.'.join(input_file.split('.')[:-1])
+    output_file += '.ass'
+
+    for ln in range(len(lines)):
+        line = lines[ln]
+        if line.isdigit() and re.match('-?\d\d:\d\d:\d\d', lines[(ln+1)]):
+            if tmpLines:
+                subLines += tmpLines + "\n"
+            tmpLines = ''
+            lineCount = 0
+            continue
+        else:
+            if re.match('-?\d\d:\d\d:\d\d', line):
+                line = line.replace('-0', '0')
+                tmpLines += 'Dialogue: 0,' + line + ',SubStyle,,0,0,0,,'
+            else:
+                if lineCount < 2:
+                    tmpLines += line
+                else:
+                    tmpLines += '\N' + line
+            lineCount += 1
+        ln += 1
+
+
+    subLines += tmpLines + "\n"
+
+    subLines = re.sub(r'\d(\d:\d{2}:\d{2}),(\d{2})\d', '\\1.\\2', subLines)
+    subLines = re.sub(r'\s+-->\s+', ',', subLines)
+    # replace style
+    subLines = re.sub(r'<([ubi])>', "{\\\\\g<1>1}", subLines)
+    subLines = re.sub(r'</([ubi])>', "{\\\\\g<1>0}", subLines)
+    subLines = re.sub(r'<font\s+color="?#(\w{2})(\w{2})(\w{2})"?>', "{\\\\c&H\\3\\2\\1&}", subLines)
+    subLines = re.sub(r'</font>', "", subLines)
+
+    head_str = '''[Script Info]
+ScriptType: v4.00+
+Collisions: Normal
+PlayDepth: 0
+WrapStyle: 0
+PlayResX: 1280
+PlayResY: 720
+WrapStyle: 0
+ScaledBorderAndShadow: yes
+[V4+ Styles]
+Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline,cls StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: Default,Noto Sans SemBd,25,&H00FFFFFF,&H000000FF,&H00020713,&H00000000,0,0,0,0,100,100,0,0,1,1,0,2,20,20,20,1
+[Events]
+Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text'''
+
+    output_str = utf8bom + head_str + '\n' + subLines
+    output_str = output_str.encode(encoding)
+
+    with open(output_file, 'wb') as output:
+        output.write(output_str)
+
+    output_file = output_file.replace('\\', '\\\\')
+    output_file = output_file.replace('/', '//')
+    return output_file
+
+
+if len(sys.argv) > 1:
+    for name in sys.argv[1:]:
+        srt2ass(name)
